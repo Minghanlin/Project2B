1918e11881cebc77a6f29f7803dbe40a60efa742
Minghan Lin <cd>
2016-07-27 12:04:41 +0800

    minor modifications idea not crystalized

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,8 +1,8 @@
 module StaticPagesHelper
 
   def heading_detail(heading)
-    base_heading = "Welcome to Twitter"
-    
+    base_heading = "Welcome to 2B"
+
     if heading != ''
       base_heading = base_heading + ' | ' + heading
     else
