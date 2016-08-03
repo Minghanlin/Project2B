4874713f8fb23d840d5562804c477a5f4fdc6765
Minghan Lin <cd>
2016-08-03 11:06:40 +0800

    review form is up

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
-end
+  def gravatar_for(user, options = { size: 80 })
+     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+     size = options[:size]
+     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size} (3KB)
+
+  "
+     image_tag(gravatar_url, alt: user.firstname, class: "gravatar")
+   end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   resources :users, except: :index
   resources :products
   resources :reviews, only: [:create, :destroy]
+  resources :products do
+    resources :reviews
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
