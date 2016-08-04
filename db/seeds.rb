16aa8785771ac82e664b9095e4bc135241f19ba1
Minghan Lin <cd>
2016-08-04 12:21:04 +0800

    took out new products for non admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,35 @@
 User.create!(name: "Admin",
-           email: "admin@admin.com",
-           password:              "adminredmart",
-           password_confirmation: "adminredmart",
-           admin: true)
+email: "admin@admin.com",
+password: "adminredmart",
+password_confirmation: "adminredmart",
+admin: true)
+
+User.create!(name: "Son Goku",
+email: "goku@goku.com",
+password: "gokuredmart",
+password_confirmation: "gokuredmart",
+admin: false)
+
+User.create!(name: "Vageta",
+email: "vegeta@vegeta.com",
+password: "vegetaredmart",
+password_confirmation: "vegetaredmart",
+admin: false)
+
+User.create!(name: "Piccolo Latte",
+email: "piccolo@piccolo.com",
+password: "piccoloredmart",
+password_confirmation: "piccoloredmart",
+admin: false)
+
+User.create!(name: "Chi Chi",
+email: "chichi@chichi.com",
+password: "chichiredmart",
+password_confirmation: "chichiredmart",
+admin: false)
+
+User.create!(name: "Bulma",
+email: "bulma@bulma.com",
+password: "bulmaredmart",
+password_confirmation: "bulmaredmart",
+admin: false)
